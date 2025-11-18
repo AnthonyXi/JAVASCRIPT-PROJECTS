150ffb1bf5ceeabcb17f609ac99a0c91e6edc921
AnthonyXi <xionganthony65609@gmail.com>
2025-11-18 15:01:27 -0800

    BASIC JAVASCRIPT PROJECTS SUBMISSION ASSIGNMENT

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,15 @@
+alert("Hello, how can i help you?"); //Added alert box
+window.alert("This is an alert box!"); //Added window alert box
+var A = "This is the beginning of the string"; //Defining variable A 
+//Defining string A
+var B = " and this is the end of the string."; //Defining variable B
+//Defining string B
+var C = A + B;                                 //Concatenating strings A and B
+document.write(C);                             //Concatenated string to document
+document.write(5 + 5);                         //Expression that adds two numbers
+function My_First_Function() {                       //Defining function and naming it
+    var str = "This is the button text";                 //Defining variable and giving it
+                                                         //string value
+    document.getElementById("Button_Text").innerHTML = str; //Putting the value of the variable
+                                                            //into HTML elementFromPoint with the "Button_Text" id
+}                                                
