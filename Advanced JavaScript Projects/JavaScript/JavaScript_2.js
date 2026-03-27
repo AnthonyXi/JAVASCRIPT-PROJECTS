8fcdad9237bf36db22542114ff968c49bb8f288b
AnthonyXi <xionganthony65609@gmail.com>
2026-03-27 06:24:37 -0700

    TIC TAC TOE Assignment

--- Advanced JavaScript Projects/JavaScript/JavaScript_2.js
+++ Advanced JavaScript Projects/JavaScript/JavaScript_2.js
@@ -1,4 +1,4 @@
-function validateForm() {
+function validateForm() {   // This function validates the form input. It checks if the "fname" field is empty and alerts the user if it is. It returns false to prevent form submission if the validation fails.
   let x = document.forms["myForm"]["fname"].value;
   if (x == "") {
     alert("Name must be filled out");
--- Advanced JavaScript Projects/JavaScript/JavaScript_3.js
+++ Advanced JavaScript Projects/JavaScript/JavaScript_3.js
@@ -1,4 +1,4 @@
-function displayType(character) {
+function displayType(character) {   // This function takes a character element as an argument, retrieves the value of the "data-character-type" attribute, and displays an alert with the character type and the universe they belong to.
     var characterType = character.getAttribute("data-character-type");
     alert(characterType + " is in the" + character.innerHTML + " universe!");
 }
